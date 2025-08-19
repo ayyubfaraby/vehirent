132a705090c6786bd0102c12bfeca78cfbd2ca17
renda-byte <rendareista@gmail.com>
2025-08-19 10:09:42 +0700

    nambah database

---
+++ app/Database/Migrations/2025-08-17-133416_CreateProductsTable.php
@@ -0,0 +1,45 @@
+<?php //perintah buat tabel
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateProductsTable extends Migration
+{
+    public function up() 
+    {
+        $this->forge->addField([ 
+            'id'          => [
+                'type'           => 'INT',
+                'constraint'     => 11,
+                'unsigned'       => true,
+                'auto_increment' => true,
+                ],
+
+            'nama'    => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'deskripsi'      => [
+                'type'       => 'VARCHAR',
+                'constraint' => '100',
+            ],
+            'gambar' => [
+                'type'          => 'VARCHAR',
+                'constraint'    => '255',
+                'null'          => true,
+            ],
+            'created_at' => [
+                'type'      => 'DATETIME',
+                'null'      => true,
+            ]
+        ]);
+        $this->forge->addKey('id', true);
+        $this->forge->createTable('products');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('products');
+    }
+}
